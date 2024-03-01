ac6b0f479df82a32a74c08ebffbb32d069ae6a1b
Jhony Pereira <jhony-16@live.com>
2024-03-01 11:50:12 -0300

    feat: add config deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  output: "export",
+};
 
 export default nextConfig;
